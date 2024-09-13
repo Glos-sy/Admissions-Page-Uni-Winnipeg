4346e55af0050bfb083e6b4c69fcbaf14109b619
Ayo <baniremubarakah@gmail.com>
2024-09-13 16:54:33 +0100

    next commit

---
+++ Task-38A/task38-a.js
@@ -0,0 +1,21 @@
+console.log("Hello");
+
+alert("How are you?");
+
+prompt("Input your name");
+
+var school = "Vephla";
+console.log(school);
+
+var age = "30";
+var matricNumber = "332";
+
+console.log(age / matricNumber);
+
+console.log(age + matricNumber);
+
+console.log(age * matricNumber);
+
+console.log(typeof school);
+
+console.log(typeof age);
