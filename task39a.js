cb05043399ff9aa54555b71f1698a9f085b4c47d
Ayo <baniremubarakah@gmail.com>
2024-10-11 08:27:50 +0100

    next commit

---
+++ task39a.js
@@ -0,0 +1,558 @@
+// console.log("Hello");
+
+// var school = "School";
+
+// // // var school = "Vephla";
+// // // console.log(school);
+
+// // // let age = 20;
+
+// // // age = 24;
+// // // console.log(age);
+
+// // // const weather = "rainy season";
+// // // console.log(weather);
+
+// // // Strings
+// // let name = "Ayo";
+// // console.log(typeof name);
+
+// // // Numbers
+// // let a = 4;
+// // console.log(typeof a);
+
+// // // Boolean
+// // let day = true;
+// // console.log(typeof day);
+
+// // // Null
+// // let reclass = null;
+// // console.log(typeof reclass);
+
+// // let farm;
+// // console.log(typeof farm);
+
+// // OPERATIONS
+// let d = "4";
+// let b = 20;
+// let c = 33;
+// let f = "100";
+
+// +, -, *, /
+// =
+
+// let y = 10;
+// // console.log(y + 10);
+
+// y /= 10;
+
+// +=, -=, *=, /=
+
+// console.log(9 / 3);
+// console.log(10 % 3);
+
+// COMPARISON OPERATORS
+// <, >, <=, >=, ==, ===, !=, !==
+// console.log(3 !== "3");
+
+// OPERATOR PRECEDENCE
+// BODMAS
+
+//
+
+// console.log(b ** c);
+// console.log(d + f);
+
+// let color = "green";
+// let myName = "Ayo";
+// let verb = "is";
+// let object = "with envy";
+
+// console.log(myName + " " + verb + " " + color + " " + object);
+// console.log(`${myName} ${verb} ${color} ${object} see`);
+
+// const a = "The";
+// let b = "cat";
+// let c = "barked";
+// let d = "lady";
+// let e = "blue";
+// let f = "lady";
+// const verb = "is";
+// let g = "food";
+// let h = "delicious";
+// let i = "loud";
+// let j = "music";
+// const punct = ".";
+// let k = "dog";
+// let l = "flowers";
+// let m = "colorful";
+// let n = "are";
+// let o = "sun";
+// let p = "shines";
+// let q = "exciting";
+// let r = "long";
+// let s = "beautiful";
+// let t = "pink";
+
+// // console.log(a + " " + b + " " + verb + " " + i);
+// // console.log(a + " " + b + " " + verb + " " + i + ".");
+// // console.log(a + " " + b + " " + verb + " " + e + ".");
+// // console.log(a + " " + f + " " + verb + " " + i + punct);
+// // console.log(a + " " + g + "  " + verb + " " + h + punct);
+// // console.log(a + " " + g + " " + verb + " " + e + punct);
+// // console.log(a + " " + k + " " + c + punct);
+// // console.log(a + " " + j + " " + verb + " " + i + punct);
+// // console.log(a + " " + l + " " + n + " " + m + punct);
+// // console.log(a + " " + l + " " + n + " " + e + punct);
+// // console.log(a + " " + l + " " + n + " " + s + punct);
+// // console.log(a + " " + l + " " + n + " " + t + punct);
+// // console.log(a + " " + j + " " + verb + " " + r + punct);
+// // console.log(a + " " + j + " " + verb + " " + s + punct);
+// // console.log(a + " " + f + " " + verb + " " + s + punct);
+// // console.log(a + " " + f + " " + p);
+// // console.log(a + " " + f + " " + verb + " " + q + punct);
+
+// console.log(`${a} ${b} ${verb} ${i}`);
+// console.log(`${a} ${b} ${verb} ${i}.`);
+// console.log(`${a} ${b} ${verb} ${e}.`);
+// console.log(`${a} ${f} ${verb} ${i}${punct}`);
+// console.log(`${a} ${g} ${verb} ${h}${punct}`);
+// console.log(`${a} ${g} ${verb} ${e}${punct}`);
+// console.log(`${a} ${k} ${c}${punct}`);
+// console.log(`${a} ${j} ${verb} ${i}${punct}`);
+// console.log(`${a} ${l} ${n} ${m}${punct}`);
+// console.log(`${a} ${l} ${n} ${e}${punct}`);
+// console.log(`${a} ${l} ${n} ${s}${punct}`);
+// console.log(`${a} ${l} ${n} ${t}${punct}`);
+// console.log(`${a} ${j} ${verb} ${r}${punct}`);
+// console.log(`${a} ${j} ${verb} ${s}${punct}`);
+// console.log(`${a} ${f} ${verb} ${s}${punct}`);
+// console.log(`${a} ${f} ${p}${punct}`);
+// console.log(`${a} ${f} ${verb} ${q}${punct}`);
+
+// TYPE CONVERSION
+// let myName = "Ayo";
+// console.log(Number(myName));
+
+// let myAge = 10;
+// let myFirstConversion = String(myAge);
+// console.log(typeof myFirstConversion);
+
+// TYPE COERSION
+// + PLUS OPERATION
+// console.log("My number is " + 300 + " and it is satisfactory.");
+// console.log("My number is " + "300" + " and it is satisfactory.");
+
+// - SUBTRACTION OPERATION
+// console.log("200" - 40);
+
+// MULTIPLICATION OPERATION
+// console.log("2000" * 3);
+
+// FALSY VALUES = NaN, "", undefined, 0, null
+// console.log(Boolean(NaN));
+// console.log(Boolean(null));
+// console.log(Boolean(""));
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+
+// If/Else Condition Statements
+// if (condition) {
+
+// } else {
+
+// }
+
+// let a = NaN;
+// if (a) {
+//   console.log("This is true.");
+// } else {
+//   console.log("This is false.");
+// }
+
+// let myNumber = 0;
+
+// if (myNumber) {
+//   console.log("This is a truthy value.");
+// } else {
+//   console.log("This is a falsy value.");
+// }
+
+// let secondNumber = NaN;
+// if (secondNumber) {
+//   console.log("This is a truthy value.");
+// } else {
+//   console.log("This is a falsy value");
+// }
+
+// let thirdNumber = undefined;
+// if (thirdNumber) {
+//   console.log("This is a truthy value.");
+// } else {
+//   console.log("This is a falsy value");
+// }
+
+// let fourthNumber = null;
+// if (fourthNumber) {
+//   console.log("This is a truthy value.");
+// } else {
+//   console.log("This is a falsy value");
+// }
+
+// let fifthNumber = "";
+// if (fifthNumber) {
+//   console.log("This is a truthy value.");
+// } else {
+//   console.log("This is a falsy value");
+// }
+
+// // !Falsy
+// let myNumberNot = !0;
+
+// if (myNumberNot) {
+//   console.log("This is a truthy value.");
+// } else {
+//   console.log("This is a falsy value.");
+// }
+
+// let secondNumberNot = !NaN;
+// if (secondNumberNot) {
+//   console.log("This is a truthy value.");
+// } else {
+//   console.log("This is a falsy value");
+// }
+
+// let thirdNumberNot = !undefined;
+// if (thirdNumberNot) {
+//   console.log("This is a truthy value.");
+// } else {
+//   console.log("This is a falsy value");
+// }
+
+// let fourthNumberNot = !null;
+// if (fourthNumberNot) {
+//   console.log("This is a truthy value.");
+// } else {
+//   console.log("This is a falsy value");
+// }
+
+// let fifthNumberNot = !"";
+// if (fifthNumberNot) {
+//   console.log("This is a truthy value.");
+// } else {
+//   console.log("This is a falsy value");
+// }
+
+// let firstAge = "";
+// if (firstAge) {
+//   console.log("This is a truthy value.");
+// } else {
+//   console.log("This is a falsy value");
+// }
+
+// LOGICAL OPERATORS
+// AND, OR, NOT
+// AND &&
+// OR ||
+// NOT !
+
+// AND
+// let isFemale = false;
+// let canDance = false;
+// console.log(isFemale && canDance);
+
+// OR
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+
+// NOT
+// console.log(!false);
+// console.log(!false && !true);
+
+// if (true && true) {
+//   console.log("This is true!");
+// } else {
+//   console.log("This is false!");
+// }
+
+// if (true || false || true) {
+//   console.log("This is true!");
+// } else {
+//   console.log("This is false");
+// }
+
+// AND
+// if (true && true) {
+//   console.log("This is true!");
+// } else {
+//   console.log("This is false!");
+// }
+
+// if (true && false) {
+//   console.log("This is true!");
+// } else {
+//   console.log("This is false!");
+// }
+
+// if (false && true) {
+//   console.log("This is true!");
+// } else {
+//   console.log("This is false!");
+// }
+
+// if (false && false) {
+//   console.log("This is true!");
+// } else {
+//   console.log("This is false!");
+// }
+
+// OR
+// if (true || true) {
+//   console.log("This is true!");
+// } else {
+//   console.log("This is false!");
+// }
+
+// if (true || false) {
+//   console.log("This is true!");
+// } else {
+//   console.log("This is false!");
+// }
+
+// if (false || true) {
+//   console.log("This is true!");
+// } else {
+//   console.log("This is false!");
+// }
+
+// if (false || false) {
+//   console.log("This is true!");
+// } else {
+//   console.log("This is false!");
+// }
+
+// NOT
+// if (!true) {
+//   console.log("This is true!");
+// } else {
+//   console.log("This is false!");
+// }
+
+// if (!false) {
+//   console.log("This is true!");
+// } else {
+//   console.log("This is false!");
+// }
+
+// if (!false && !false) {
+//   console.log("This is true!");
+// } else {
+//   console.log("This is false!");
+// }
+
+// if (!false || !false) {
+//   console.log("This is true!");
+// } else {
+//   console.log("This is false!");
+// }AyoA
+
+// if (true && true && true) {
+//   console.log("This is true!");
+// } else {
+//   console.log("This is false!");
+// }
+
+// let loginCredentials = prompt("What is your name?");
+// const myName = "Ayobomi";
+// // const password = "Ayo";
+// let lastName = "Banire";
+
+// if (loginCredentials === myName) {
+//   let myPrompt = prompt("Enter your password");
+//   if (myPrompt === lastName) {
+//     console.log("Welcome");
+//   } else if (myPrompt === "") {
+//     console.log("Cancelled");
+//   } else {
+//     console.log("Wrong Password");
+//   }
+// } else if (loginCredentials === " ") {
+//   console.log("Invalid");
+// } else if (loginCredentials === "") {
+//   console.log("Cancelled");
+// } else {
+//   console.log("I don't know you.");
+// }
+
+// let loginCredentials = prompt("What is your name?");
+// const firstName = "Ayobomi";
+// const lastName = "Banire";
+
+// if (loginCredentials === firstName) {
+//   let mySecondPrompt = prompt("What is your password?");
+//   if (mySecondPrompt === lastName) {
+//     console.log(alert("Welcome!"));
+//   } else if (mySecondPrompt === "") {
+//     console.log(alert("Cancelled"));
+//   } else {
+//     console.log(alert("Wrong Password!"));
+//   }
+// } else if (loginCredentials === "") {
+//   console.log(alert("Cancelled!"));
+// } else if (loginCredentials === " ") {
+//   console.log(alert("Invalid"));
+// } else {
+//   console.log(alert("I don't know you."));
+// }
+
+// OPERATOR PRECEDENCE
+// + - * /
+// += -= *= /=
+// == === != !==
+// < > <= >=
+
+// && || !
+// !, &&, ||
+// === !== == != ! && ||
+// console.log((3 === 3 && 3 + 4) || 3 === "3");
+// console.log((true && 7) || false);
+// console.log((3 + 5 == 5 - 2) !== 5);s
+
+// const age = 30;
+// let gender = "female";
+
+// if (age === 30) {
+//   console.log("Hey Millenial");
+//   if (gender === "male") {
+//     console.log("Hey Millenial Guy");
+//   } else {
+//     console.log("Hey Millenial Girl");
+//   }
+// } else if (age >= 20) {
+//   console.log("Hey GenZ");
+// } else if (age >= 12) {
+//   console.log("Hey Alpha 0");
+// } else if (age > 30) {
+//   console.log("Hey Oldie");
+// } else {
+//   console.log("Who are you?");
+// }
+
+// switch (age) {
+//   case 30:
+//     console.log("Hey Millenial");
+//     switch (gender) {
+//       case "male":
+//         console.log("Hey Millenial Guy");
+//         break;
+//       case "female":
+//         console.log("Hey Millenial Girl");
+//         break;
+//       default:
+//         console.log("Invalid Gender");
+//         break;
+//     }
+//     break;
+//   case 20:
+//     console.log("Hey GenZ");
+//     break;
+//   case 12:
+//     console.log("Hey Alpha 0");
+//     break;
+//   default:
+//     console.log("Who are you?");
+//     break;
+// }
+
+// let x = 3 + 5;
+// console.log(x);
+
+// function tryout(age) {
+//   console.log(age);
+// }
+
+// tryout(20);
+
+// Expressions and Statements
+// Expressions:
+// let sample = 35 % 4;
+// console.log(sample);
+
+// // Statements:
+// if (sample === 3) {
+//   let myAge = 3 * 10;
+//   console.log(myAge);
+// } else {
+//   console.log("Invalid");
+// }
+
+// Tenary Operators
+// condition ? value 1 : value 2
+// let summation = 1 < 4 ? console.log("True") : console.log("False");
+
+// condition 1 ? value 1 : condition 2 ? value 2 : condition 3 ? value 3:
+// defaultValue
+
+// let myDisplay = 30;
+// if (myDisplay >= 30)
+//   ? console.log("You are aged")
+//   : myDisplay >= 20
+//   ? console.log("You are okay")
+//   : myDisplay >= 10
+//   ? console.log("Hey minor")
+// : console.log("Hey stranger");
+
+// let age = 20;
+// let individual =
+//   age > 20
+//     ? console.log("Hey aged")
+//     : (age = 20 ? console.log("You are okay") : console.log("Hey minor"));
+
+// let age = 30;
+// let individualAge =
+//   age > 40
+//     ? console.log("You are aged")
+//     : age >= 30
+//     ? console.log("You are average")
+//     : age < 20
+//     ? console.log("you are too small")
+//     : console.log("Bye Minor");
+
+// FUNCTIONS
+// function name(params) {}
+
+// function myFirstFunction(func1, func2) {
+//   console.log(func1, func2);
+// }
+// myFirstFunction(3, 5);
+
+// let day = "Wednesday";
+// if (day === "Monday") {
+//   console.log("Have a good Monday!");
+// } else if (day === "Tuesday") {
+//   console.log("Have a good Tuesday!");
+// } else if (day === "Wednesday") {
+//   console.log("Have a good Wednesday!");
+// } else if (day === "Thursday") {
+//   console.log("Have a good Thursday!");
+// } else {
+//   console.log("Have a great weekend!");
+// }
+
+// let checkOut =
+//   day === "Monday"
+//     ? console.log("Have a good Monday!")
+//     : day === "Tuesday"
+//     ? console.log("Have a good Tuesday!")
+//     : day === "Wednesday"
+//     ? console.log("Have a good Wednesday!")
+//     : day === "Thursday"
+//     ? console.log("Have a good Thursday!")
+//     : console.log("Have a great weekend!");
+
+function sampleFunction(param1, param2, param3) {
+  console.log(param1, param2, param3);
+}
+sampleFunction("Monday", "Tuesday", "Wednesday");
